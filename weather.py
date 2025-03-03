df22f18358c3cdd5db7f46d39ec1b3d1d15f8580
Komal Kumari <komalkapoor0806@gmail.com>
2025-03-03 15:56:54 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+import requests
+
+api_url = "http://api.weatherapi.com/v1/current.json?"
+api_key = "0c969b83b3324359b5685710250303"
+
+st.title("Weather App")
+
+city = st.text_input("Enter a city name:")
+
+if city:
+    url = api_url + "key=" + api_key + "&q=" + city
+    res = requests.get(url)
+    if res.status_code == 200:
+        data = res.json()
+        st.write(f"The current Temperature in {city} is : {data['current']['temp_c']} °C")
+        st.write(data['current'])
+    else:
+        st.write("City not found")
